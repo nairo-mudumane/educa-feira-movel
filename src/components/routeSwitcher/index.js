03b41c7dc217d4a0738ddda300a4b1fa52e2e01d
NairoMudumane <nairomudumane@outlook.com>
2021-09-20 14:45:45 +0200

    adding timer

--- src/components/routeSwitcher/index.js
+++ src/components/routeSwitcher/index.js
@@ -7,17 +7,37 @@ const StyledSwitcher = styled.div`
 `;
 
 export default function RouteSwitcher() {
+  const links = [];
+  //   let timer = '3000';
+  //   const [count, setCount] = React.useState(0);
   const hrefs = [
-    { to: '/', class: 'home' },
-    { to: 'info', class: 'info' },
-    { to: 'quiz', class: 'quiz' },
+    { to: '/', id: 'home' },
+    { to: 'info', id: 'info' },
+    { to: 'quiz', id: 'quiz' },
   ];
+  hrefs.forEach((link) => {
+    links.push(document.getElementById(link.id));
+  });
+
+  const [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      if (count >= hrefs.length) {
+        setCount((count) => (count = -1));
+      }
+      setCount((count) => count + 1);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, [count, hrefs.length]);
 
   return (
     <StyledSwitcher>
       {hrefs.map((href) => (
-        <NavLink to={href.to} className={href.class} key={href.to} />
+        <NavLink to={href.to} id={href.id} key={href.to}>
+          {href.id}
+        </NavLink>
       ))}
+      <p>{count}</p>
     </StyledSwitcher>
   );
 }
