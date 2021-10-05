9a10686f9784fcd5f1a4510f0200463cdce52c71
NairoMudumane <nairomudumane@outlook.com>
2021-10-05 13:50:11 +0200

    fixing effect erros

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react';
 import Loader from '../../components/loader';
 import BgWhite from '../../layout/wrappers/BgWhite';
@@ -9,34 +8,8 @@ import InfoBody from './InfoBody';
 import InfoTitle from './InfoTitle';
 import Body from '../../layout/wrappers/Body';
 
-export default function Info() {
-  const [data, setData] = React.useState(null);
-  const [error, setError] = React.useState(false);
-  const [load, setLoad] = React.useState(false);
-
-  try {
-    React.useEffect(() => {
-      async function getInfo() {
-        setLoad(true);
-        await axios
-          .get('https://api-educa-movel.herokuapp.com/questions')
-          .then((response) => {
-            setData(response.data);
-            filterInfo(response.data);
-          });
-        setLoad(false);
-      }
-      getInfo();
-    }, []);
-  } catch (err) {
-    setError(err);
-  }
-
-  function filterInfo({ data }) {
-    console.log(data);
-    return null;
-  }
-
+export default function Info({ load, data, error }) {
+  console.log(data.data[0]);
   if (error)
     return (
       <Body>
@@ -52,17 +25,19 @@ export default function Info() {
   return (
     <Body>
       <Container>
-        <Title title="Sabia que ..." />
+        <Title title="Sabia que..." />
         {load ? (
           <Error>
             <Loader />
           </Error>
-        ) : data ? (
-          <BgWhite>
-            <InfoTitle title="{data[0].title}" />
-            <InfoBody body="{data[0].content}" />
-          </BgWhite>
-        ) : null}
+        ) : (
+          data && (
+            <BgWhite>
+              <InfoTitle title={data.data[0].title} />
+              <InfoBody body={data.data[0].content} />
+            </BgWhite>
+          )
+        )}
       </Container>
     </Body>
   );
--- src/pages/slider/index.js
+++ src/pages/slider/index.js
@@ -6,6 +6,7 @@ import Main from '../../layout/wrappers/Main';
 import Welcome from '../home';
 import Info from '../info/';
 import Quiz from '../quiz/';
+import axios from 'axios';
 
 const StyledSlider = styled.div`
   max-height: 80vh;
@@ -22,18 +23,38 @@ const StyledSlider = styled.div`
 `;
 
 export default function Slider() {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(false);
+  const [load, setLoad] = React.useState(false);
+  try {
+    React.useEffect(() => {
+      async function getInfo() {
+        setLoad(true);
+        await axios
+          .get('https://api-educa-movel.herokuapp.com/questions')
+          .then((response) => {
+            setData(response.data);
+          });
+        setLoad(false);
+      }
+      getInfo();
+    }, []);
+  } catch (err) {
+    setError(err);
+  }
   return (
     <Main>
       <StyledSlider>
         <Carousel
           interval={1000}
-          infiniteLoop={true}
-          autoPlay={true}
-          showArrows={false}
+          infiniteLoop
+          autoPlay={false}
+          showArrows
           showIndicators={false}
+          showThumbs={false}
         >
           <Welcome />
-          <Info />
+          {data ? <Info load={load} data={data} error={error} /> : null}
           <Quiz />
         </Carousel>
       </StyledSlider>
